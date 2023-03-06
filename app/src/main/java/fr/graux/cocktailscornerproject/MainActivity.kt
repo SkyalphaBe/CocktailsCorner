5aeffbd7da8e91ec8b75bc925ddabad447591ef6
Segalla Valentin <valentin.segalla@etu.univ-lyon1.fr>
2023-03-06 10:25:07 +0100

    génération map avec API Google

--- app/src/main/java/fr/graux/cocktailscornerproject/MainActivity.kt
+++ app/src/main/java/fr/graux/cocktailscornerproject/MainActivity.kt
@@ -1,11 +1,32 @@
 package fr.graux.cocktailscornerproject
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import fr.graux.cocktailscornerproject.databinding.ActivityMainBinding
+
+
+class MainActivity : FragmentActivity(), OnMapReadyCallback{
+
+    private lateinit var mMap:GoogleMap
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment:SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment;
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val sydney:LatLng = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
