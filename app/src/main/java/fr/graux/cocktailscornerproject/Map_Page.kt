23aaa79fd6c67d9d262df7c7a44aa1a5995ecb2a
Segalla Valentin <valentin.segalla@etu.univ-lyon1.fr>
2023-03-06 11:38:32 +0100

    adaptation Google Map view avec Fragment

--- app/src/main/java/fr/graux/cocktailscornerproject/Map_Page.kt
+++ app/src/main/java/fr/graux/cocktailscornerproject/Map_Page.kt
@@ -1,8 +1,36 @@
 package fr.graux.cocktailscornerproject
 
-
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import fr.graux.cocktailscornerproject.databinding.ActivityMainBinding
 
-class Map_Page : Fragment(R.layout.fragment_map__page) {
+class Map_Page : Fragment(), OnMapReadyCallback{
 
+    private lateinit var mMap:GoogleMap
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view:View = inflater.inflate(R.layout.fragment_map__page,container,false)
+
+        val mapFragment:SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        return view
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val sydney:LatLng = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 }
