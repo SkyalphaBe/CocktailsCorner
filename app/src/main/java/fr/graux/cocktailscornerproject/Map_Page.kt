c91be5ef9c8a411554e0b647a47bd75496631b7e
Segalla Valentin <valentin.segalla@etu.univ-lyon1.fr>
2023-03-11 11:58:00 +0100

    affichage de la position actuelle de l'utilisateur sur la map (gestion des permissions)

--- app/src/main/java/fr/graux/cocktailscornerproject/Map_Page.kt
+++ app/src/main/java/fr/graux/cocktailscornerproject/Map_Page.kt
@@ -1,19 +1,24 @@
 package fr.graux.cocktailscornerproject
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import fr.graux.cocktailscornerproject.databinding.ActivityMainBinding
 
-class Map_Page : Fragment(), OnMapReadyCallback{
+class Map_Page : Fragment(),
+    OnMapReadyCallback,
+    ActivityCompat.OnRequestPermissionsResultCallback{
 
     private lateinit var mMap:GoogleMap
-    private lateinit var binding: ActivityMainBinding
+    private var PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION:Int = 1
+    private var locationPermissionGranted:Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,8 +34,66 @@ class Map_Page : Fragment(), OnMapReadyCallback{
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        val sydney:LatLng = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        updateLocationUI()
+    }
+
+    private fun getLocationPermission(){
+        if (this.context?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED && this.context?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED
+        ) {
+             requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+        else{
+            locationPermissionGranted=true
+            updateLocationUI()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION ->{
+                if(grantResults.isNotEmpty()&&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    locationPermissionGranted=true
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+        updateLocationUI()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationUI() {
+        try {
+            if (locationPermissionGranted) {
+                mMap.isMyLocationEnabled = true
+                mMap.uiSettings.isMyLocationButtonEnabled = true
+                zoomToCurrentLocation()
+            } else {
+                mMap.isMyLocationEnabled = false
+                mMap.uiSettings.isMyLocationButtonEnabled = false
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    private fun zoomToCurrentLocation(){
+
     }
 }
