e00a0e95a764d64789afe6167102d96f035214a0
Segalla Valentin <valentin.segalla@etu.univ-lyon1.fr>
2023-03-13 21:39:48 +0100

    style light/dark thème navBar + bug fix fragment Map

--- app/src/main/java/fr/graux/cocktailscornerproject/Setting_Page.kt
+++ app/src/main/java/fr/graux/cocktailscornerproject/Setting_Page.kt
@@ -6,25 +6,48 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_setting__page.*
 
 class Setting_Page : Fragment(R.layout.fragment_setting__page) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         checkUItheme()
 
         darkModeSwitch.setOnCheckedChangeListener {buttonView, isChecked->
-            if (isChecked)
+            val bottomNavView = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            else
+                bottomNavView.background = ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.bottom_nav_backgound_dark
+                )
+                bottomNavView.itemTextColor=ContextCompat
+                    .getColorStateList(requireContext(),R.drawable.color_item_nav_dark)
+                bottomNavView.itemIconTintList=ContextCompat
+                    .getColorStateList(requireContext(),R.drawable.color_item_nav_dark)
+            }else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                bottomNavView.background = ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.bottom_nav_backgound
+                )
+                bottomNavView.itemTextColor=ContextCompat
+                    .getColorStateList(requireContext(),R.drawable.color_item_nav)
+                bottomNavView.itemIconTintList=ContextCompat
+                    .getColorStateList(requireContext(),R.drawable.color_item_nav)
+            }
         }
     }
 
-    fun checkUItheme(){
-        val uiModeManager:UiModeManager = requireContext().getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+    private fun checkUItheme(){
+        val uiModeManager:UiModeManager = requireContext().getSystemService(Context.UI_MODE_SERVICE)
+                as UiModeManager
         val mode:Int = uiModeManager.nightMode
         if(mode == UiModeManager.MODE_NIGHT_YES){
             darkModeSwitch.isChecked=true
