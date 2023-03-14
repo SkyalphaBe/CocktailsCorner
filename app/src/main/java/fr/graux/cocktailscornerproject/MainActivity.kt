3e5684f1218a80e4b1cfa3fbe0625da1314014f5
François Graux <frsgra28@gmail.com>
2023-03-14 13:28:22 +0100

    notification sans firebase debut

--- app/src/main/java/fr/graux/cocktailscornerproject/MainActivity.kt
+++ app/src/main/java/fr/graux/cocktailscornerproject/MainActivity.kt
@@ -1,5 +1,14 @@
 package fr.graux.cocktailscornerproject
 
+
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.content.Intent
 import android.app.UiModeManager
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +16,7 @@ import android.os.Bundle
 import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.viewbinding.ViewBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import fr.graux.cocktailscornerproject.databinding.ActivityMainBinding
 
@@ -14,6 +24,12 @@ import fr.graux.cocktailscornerproject.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var notificationManager: NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: Notification.Builder
+    private val channelId = "i.apps.notifications"
+    private val description = "Test notification"
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,23 +39,41 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-
-
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragment)
 
-        bottomNavView.setupWithNavController(navController)
+        val intent = Intent(this, ReminderBroadcast::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(this,0,intent,FLAG_IMMUTABLE)
 
+        bottomNavView.setupWithNavController(navController)
+        createNotificationChannel()
+        var alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val time = System.currentTimeMillis()
+        val tenSec = 1000*10
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP,time+tenSec,pendingIntent)
+    }
+
+    @SuppressLint("ResourceType")
+    fun createNotificationChannel(){
+        val name = "notifChannel"
+        val desc = "channel pour notif "
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel("channelID",name,importance)
+        channel.description=desc
+
+        val notifManager = getSystemService(NotificationManager::class.java)
+        notifManager.createNotificationChannel(channel)
         val uiModeManager: UiModeManager = applicationContext
             .getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
         val mode:Int = uiModeManager.nightMode
 
         if(mode == UiModeManager.MODE_NIGHT_YES){
-            bottomNavView.background=ContextCompat
+            binding.bottomNavigationView.background=ContextCompat
                 .getDrawable(this,R.drawable.bottom_nav_backgound_dark)
-            bottomNavView.itemTextColor=ContextCompat
+            binding.bottomNavigationView.itemTextColor=ContextCompat
                 .getColorStateList(this,R.drawable.color_item_nav_dark)
-            bottomNavView.itemIconTintList=ContextCompat
+            binding.bottomNavigationView.itemIconTintList=ContextCompat
                 .getColorStateList(this,R.drawable.color_item_nav_dark)
         }
     }
